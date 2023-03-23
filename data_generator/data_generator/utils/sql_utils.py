e8302f088f0a9479089ac289b53a835793140022
Lignio <olfw@itu.dk>
2023-03-23 16:38:25 +0100

    making database changes now

--- data_generator/data_generator/utils/sql_utils.py
+++ data_generator/data_generator/utils/sql_utils.py
@@ -3,6 +3,8 @@ from sqlmodel import create_engine
 
 from data_generator.utils.utility import get_env_variables
 from data_generator.database.tables import Logs
+from data_generator.database.tables import Anomalies
+
 
 def get_database_connection_string():
     env = get_env_variables()
@@ -11,19 +13,24 @@ def get_database_connection_string():
     database = env["DATABASE_NAME"]
     username = env["DATABASE_USERNAME"]
     password = env["DATABASE_PASSWORD"]
-    
-    connection_string = f"postgresql+psycopg2://{username}:{password}@{hostname}:{port}/{database}"
+
+    connection_string = (
+        f"postgresql+psycopg2://{username}:{password}@{hostname}:{port}/{database}"
+    )
     return connection_string
+
 
 def get_database_engine():
     connection_string = get_database_connection_string()
     engine = create_engine(connection_string)
     return engine
 
+
 def empty_database_tables():
     engine = get_database_engine()
     with Session(engine) as session:
         delete_table = delete(Logs)
+        delete_anomalies = delete(Anomalies)
         session.exec(delete_table)
+        session.exec(delete_anomalies)
         session.commit()
-    
--- sql/logs.sql
+++ sql/logs.sql
@@ -8,20 +8,3 @@ TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public."logs"
     OWNER to postgres;
-
-CREATE TABLE IF NOT EXISTS public.anomalies (
-    "id" serial NOT NULL,
-    "log_time" text,
-    "log_message" text,
-    "anomaly_score" float,
-    "false_positive" boolean,
-    CONSTRAINT "logs_pkey" PRIMARY KEY ("id")
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public."anomalies"
-    OWNER to postgres;
-
-
-CREATE INDEX ON public.anomalies (id);
