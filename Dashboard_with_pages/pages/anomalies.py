264d6d1dde6f3ca7d68df74937310fd0179d762f
Maxime Birkkjær Havez <maxih@hotmail.dk>
2023-03-30 13:57:02 +0200

    Added dropdown for selecting time interval of anomalies
    - Callback for updating datatable based on dropdown

--- Dashboard_with_pages/pages/anomalies.py
+++ Dashboard_with_pages/pages/anomalies.py
@@ -7,60 +7,96 @@ from datetime import date
 import dash_bootstrap_components as dbc
 import random
 from plotly.graph_objs import *
-from .testdata import genLists
+from pages.testdata import genLists
+from datetime import datetime, timedelta
 
 
 dash.register_page(__name__)
 
-#Not an actual page yet. Just for showing that pages can be changed.
-testDf = pd.read_csv('Dashboard_with_pages\TestCSVLg.csv',delimiter=';')
+# Not an actual page yet. Just for showing that pages can be changed.
+orinalDf = pd.read_csv("Dashboard_with_pages\TestCSVLg.csv", delimiter=";")
+copyDf = orinalDf
 
 
-layout = html.Div(children=[
-    html.Div(
-            html.H1("Anomalies"),
-            id="TitleDIV"
-        ),
-
+layout = html.Div(
+    children=[
+        html.Div(html.H1("Anomalies"), id="TitleDIV"),
         html.Div(
-            #This breadcrumb is simply hardcoded for now. Should probably be fixed
+            # This breadcrumb is simply hardcoded for now. Should probably be fixed
             html.P("Homepage / page / page")
         ),
-
-        html.Div(children=[
-            #The datepicker is taken from the DCC page, and it's functionality is defined
-            #in the callback in the bottom. 
-            dcc.DatePickerRange(
-            id='my-date-picker-range',
-            min_date_allowed=date(1995, 8, 5),
-            max_date_allowed=date(2017, 9, 19),
-            initial_visible_month=date(2017, 8, 5),
-            end_date=date(2017, 8, 25),
-            ),
-            html.Div(id='output-container-date-picker-range',style={})
-        ]),
-
-        html.Div(children=[
-                    html.H5("Anomalies", style={"margin-top" : "20px"}),
-                    dash_table.DataTable(
-                        testDf.to_dict('records'),
-                        id="InboxTable",
-                        columns=[{"name": i, "id": i} for i in testDf.columns],
-                        editable=True,
-                        sort_action="native",
-                        sort_mode='multi',
-                        style_table={
-                        'overflow': 'auto',
-                        'height' : '100%',
-                        'width' : '100%',
-                        'marginBottom' : '20px'
-                        },
-                        style_header={
-                        'backgroundColor': '#b3b3b3',
-                        'fontWeight': 'bold'
-                        })
+        html.Div(
+            children=[
+                dcc.Dropdown(
+                    [
+                        "All time",
+                        "Today",
+                        "Yesterday",
+                        "Last two days",
+                        "This week",
                     ],
-                    style={"margin":"15px","background-color":"#e0e0d1","height":"8%","width":"70vw","outline-color" :"#b7b795","outline-style" : "solid", "outline-width" : "3px"}
-                )
+                    "Today",
+                    id="interval_picker_dropdown",
+                ),
+                html.Div(id="interval_output"),
+            ]
+        ),
+        html.Div(
+            children=[
+                html.H5("Anomalies", style={"margin-top": "20px"}),
+                dash_table.DataTable(
+                    copyDf.to_dict("records"),
+                    id="InboxTable",
+                    columns=[{"name": i, "id": i} for i in copyDf.columns],
+                    editable=True,
+                    sort_action="native",
+                    sort_mode="multi",
+                    style_table={
+                        "overflow": "auto",
+                        "height": "100%",
+                        "width": "100%",
+                        "marginBottom": "20px",
+                    },
+                    style_header={"backgroundColor": "#b3b3b3", "fontWeight": "bold"},
+                ),
+            ],
+            style={
+                "margin": "15px",
+                "background-color": "#e0e0d1",
+                "height": "8%",
+                "width": "70vw",
+                "outline-color": "#b7b795",
+                "outline-style": "solid",
+                "outline-width": "3px",
+            },
+        ),
+    ],
+    style={"margin-left": "20px"},
+)
 
-],style={"margin-left" : "20px"})
+
+def calculate_interval(value):
+    today = date.today()
+    match value:
+        case "Today":
+            return (today, today)
+        case "Yesterday":
+            return (today - timedelta(days=1), today - timedelta(days=1))
+        case "Last two days":
+            return (today, today - timedelta(days=1))
+        case "This week":
+            return (today, today - timedelta(days=6))
+
+
+def is_in_interval(interval, dateDiscovered):
+    return interval[0] <= dateDiscovered <= interval[1]
+
+
+@callback(
+    Output("interval_output", "children"), Input("interval_picker_dropdown", "value")
+)
+def adjust_table_to_interval(value):
+    interval = calculate_interval(value)
+
+    copyDf = orinalDf[is_in_interval(interval, orinalDf.Date)]
+    return 0
