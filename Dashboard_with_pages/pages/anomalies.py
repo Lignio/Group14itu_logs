9831dcc67a0dcab6657c8ba5ef8b53ef05af9163
Adam Hadou Temsamani <ahad@itu.dk>
2023-04-13 11:59:34 +0200

    Updated CSS on anomalies website

--- Dashboard_with_pages/pages/anomalies.py
+++ Dashboard_with_pages/pages/anomalies.py
@@ -18,13 +18,24 @@ testDf = pd.read_csv('Dashboard_with_pages\TestCSVLg.csv',delimiter=';')
 
 layout = html.Div(children=[
     html.Div(
-            html.H1("Anomalies"),
+            html.H1("Anomalies", className="FontBold"),
             id="TitleDIV"
         ),
 
         html.Div(
-            #This breadcrumb is simply hardcoded for now. Should probably be fixed
-            html.P("Homepage / page / page")
+            #This is the breadcrumb, made using Boostrap.
+            #The current href's lead nowhere, but can be easily changed to do so.
+            html.Nav(
+                html.Ol(className="breadcrumb", children=[
+                    html.Li(className="breadcrumb-item", children=[
+                        html.A("Home", href="./home.py", style={"text-decoration":"none", "color":"#6c757d"})
+                    ]),
+                    html.Li(className="breadcrumb-item", children=[
+                        html.A("Anomaly Detector", href="", style={"text-decoration":"none", "color":"#6c757d"})
+                    ]),
+                    html.Li("Anomalies", className="breadcrumb-item active FontBold", style={"color":"black"})
+                ])
+            )
         ),
 
         html.Div(children=[
@@ -41,7 +52,44 @@ layout = html.Div(children=[
         ]),
 
         html.Div(children=[
-                    html.H5("Anomalies", style={"margin-top" : "20px"}),
+                    html.Div(children=[
+                        html.I(className="bi bi-filter fa-2x cardLine IconBold", style={"float":"left", "margin-left":"5px","margin-right":"15px","margin-top":"-5px"}),
+                        dbc.DropdownMenu(
+                            label="Status", 
+                            toggle_style={"background":"#f8f8f8", "color":"black"}, 
+                            toggleClassName="border-white",
+                            direction="down",
+                            children=[
+                                dbc.DropdownMenuItem("Status 1", id="status_one_option"),
+                                dbc.DropdownMenuItem("Status 2", id="status_two_option"),
+                                dbc.DropdownMenuItem("Status 3", id="status_three_option" )
+                                ], className="cardLine", id="dropdownmenu_status",style={"margin-right":"8px"}),
+                        dbc.DropdownMenu(
+                            label="Severity", 
+                            toggle_style={"background":"#f8f8f8", "color":"black","border":"#f8f8f8"}, 
+                            toggleClassName="",
+                            direction="down",
+                            children=[
+                                dbc.DropdownMenuItem("Severity 1", id="severity_one_option"),
+                                dbc.DropdownMenuItem("Severity 2", id="severity_two_option"),
+                                dbc.DropdownMenuItem("Severity 3", id="severity_three_option" )
+                                ], className="cardLine", id="dropdownmenu_status",style={"margin-right":"8px"}),
+                        dbc.DropdownMenu(
+                            label=" Date detected", 
+                            toggle_style={"background":"#f8f8f8", "color":"black"}, 
+                            toggleClassName="border-white",
+                            direction="down",
+                            children=[
+                                dbc.DropdownMenuItem("Date 1", id="date_one_option"),
+                                dbc.DropdownMenuItem("Date 2", id="date_two_option"),
+                                dbc.DropdownMenuItem("Date 3", id="date_three_option" )
+                                ], className="cardLine", id="dropdownmenu_status",style={"margin-right":"8px"}),
+                            dbc.Input(id="input", className="bi bi-search fa-2x cardLine", placeholder="Search for an anomaly...", type="text",
+                                    style={"width":"25vw","float":"right","background":"#f8f8f8"})
+                        ,
+                        
+                    ], style={"margin":"10px 10px 10px 10px"}),
+                    
                     dash_table.DataTable(
                         testDf.to_dict('records'),
                         id="InboxTable",
@@ -50,17 +98,18 @@ layout = html.Div(children=[
                         sort_action="native",
                         sort_mode='multi',
                         style_table={
-                        'overflow': 'auto',
-                        'height' : '100%',
-                        'width' : '100%',
+                        'overflow' : 'auto',
+                        'padding' : '20px 20px 20px 20px',
                         'marginBottom' : '20px'
                         },
                         style_header={
-                        'backgroundColor': '#b3b3b3',
+                        'background' : '#f8f8f8',
+                        "color" : "black",
                         'fontWeight': 'bold'
                         })
                     ],
-                    style={"margin":"15px","background-color":"#e0e0d1","height":"8%","width":"70vw","outline-color" :"#b7b795","outline-style" : "solid", "outline-width" : "3px"}
+                    className="card bg-white DropShadow",
+                    style={"margin":"15px","height":"8%","width":"70vw"}
                 )
 
-],style={"margin-left" : "20px"})
+],style={"padding-left":"30px","padding-top":"20px","background-color":"#f0f3f6"})
