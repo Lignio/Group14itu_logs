34720db6a438796a2c4bb8110f5fa3cff77205fe
Lignio <olfw@itu.dk>
2023-04-19 15:53:19 +0200

    added real data to datatable, added backend functionallu to marking/ummarking anomalies as false positives

--- Dashboard_with_pages/pages/anomalies.py
+++ Dashboard_with_pages/pages/anomalies.py
@@ -1,3 +1,4 @@
+import json
 from turtle import st
 from dash import Dash, dcc, html, Input, Output, callback, dash_table, State, ctx
 import pandas as pd
@@ -7,6 +8,8 @@ from datetime import date
 import dash_bootstrap_components as dbc
 import random
 from plotly.graph_objs import *
+import requests
+from flask import request
 from pages.testdata import genLists
 from datetime import datetime, timedelta
 
@@ -14,115 +17,124 @@ from datetime import datetime, timedelta
 dash.register_page(__name__)
 
 
-# Not an actual page yet. Just for showing that pages can be changed.
-originalDF = pd.read_csv("Dashboard_with_pages\TestCSVLg.csv", delimiter=";")
-originalDF["Date"] = pd.to_datetime(
-    originalDF["Date"], format="%d/%m/%Y", dayfirst=True
-)
-
-
-# Appends the ... button to the dataframe containing the data from the database
-# This is currently using data from the  TestCSVLg file.
-buttonList = []
-for i in originalDF.index:
-    buttonList.append("...")
-
-originalDF["..."] = buttonList
-
-layout = html.Div(
-    children=[
-        html.Div(html.H1("Anomalies"), id="TitleDIV"),
-        html.Div(
-            # This breadcrumb is simply hardcoded for now. Should probably be fixed
-            html.P("Homepage / page / page")
-        ),
-        html.Div(
-            children=[
-                dcc.Dropdown(
-                    [
-                        "All time",
+def serve_layout():
+    actualDataDF = getDataDF()
+    # Render the layout.
+    return html.Div(
+        children=[
+            # dcc.Location(id='url'),
+            html.Div(html.H1("Anomalies"), id="TitleDIV"),
+            html.Div(
+                # This breadcrumb is simply hardcoded for now. Should probably be fixed
+                html.P("Homepage / page / page")
+            ),
+            html.Div(
+                children=[
+                    dcc.Dropdown(
+                        [
+                            "All time",
+                            "Today",
+                            "Yesterday",
+                            "Last two days",
+                            "Last 7 days",
+                            "This month",
+                        ],
                         "Today",
-                        "Yesterday",
-                        "Last two days",
-                        "Last 7 days",
-                        "This month",
-                    ],
-                    "Today",
-                    id="interval_picker_dropdown",
-                ),
-            ]
-        ),
-        # This is the popup menu that is shown when the user presses the ... button.
-        html.Div(
-            [
-                dbc.Modal(
-                    [
-                        dbc.ModalHeader(dbc.ModalTitle("Options")),
-                        dbc.ModalBody(
-                            html.Div(
-                                children=[
-                                    # This is the dropdown menu containing the options the user can choose
-                                    # in regards to marking/unmarking false positives.
-                                    dcc.Dropdown(
-                                        [
+                        id="interval_picker_dropdown",
+                    ),
+                ]
+            ),
+            # This is the popup menu that is shown when the user presses the ... button.
+            html.Div(
+                [
+                    dbc.Modal(
+                        [
+                            dbc.ModalHeader(dbc.ModalTitle("Options")),
+                            dbc.ModalBody(
+                                html.Div(
+                                    children=[
+                                        # This is the dropdown menu containing the options the user can choose
+                                        # in regards to marking/unmarking false positives.
+                                        dcc.Dropdown(
+                                            [
+                                                "Mark as False Positive",
+                                                "Unmark as False Positive",
+                                            ],
                                             "Mark as False Positive",
-                                            "Unmark as False Positive",
-                                        ],
-                                        "Mark as False Positive",
-                                        id="demo-dropdown",
+                                            id="demo-dropdown",
+                                        ),
+                                    ]
+                                ),
+                            ),
+                            # This is the closing buttons for the popup - OK to confirm the chosen marking of an anomaly/cancel to cancel.
+                            dbc.ModalFooter(
+                                children=[
+                                    html.A(
+                                        dbc.Button(
+                                            "OK",
+                                            id="OK",
+                                            className="ms-auto",
+                                            n_clicks=0,
+                                            href="/anomalies/",
+                                        )
+                                    ),
+                                    dbc.Button(
+                                        "Close",
+                                        id="close",
+                                        className="ms-auto",
+                                        n_clicks=0,
                                     ),
                                 ]
                             ),
-                        ),
-                        # This is the closing buttons for the popup - OK to confirm the chosen marking of an anomaly/cancel to cancel.
-                        dbc.ModalFooter(
-                            children=[
-                                dbc.Button(
-                                    "OK", id="OK", className="ms-auto", n_clicks=0
-                                ),
-                                dbc.Button(
-                                    "Close", id="close", className="ms-auto", n_clicks=0
-                                ),
-                            ]
-                        ),
-                    ],
-                    id="modal",
-                    is_open=False,
-                ),
-            ]
-        ),
-        # This is the div containing the anomalies data
-        html.Div(
-            children=[
-                html.H5("Anomalies", style={"margin-top": "20px"}),
-                dash_table.DataTable(
-                    id="InboxTable",
-                    columns=[{"name": i, "id": i} for i in originalDF.columns],
-                    editable=True,
-                    sort_action="native",
-                    sort_mode="multi",
-                    style_table={
-                        "overflow": "auto",
-                        "height": "100%",
-                        "width": "100%",
-                        "marginBottom": "20px",
-                    },
-                    style_header={"backgroundColor": "#b3b3b3", "fontWeight": "bold"},
-                ),
-            ],
-            style={
-                "margin": "15px",
-                "background-color": "#e0e0d1",
-                "height": "8%",
-                "width": "70vw",
-                "outline-color": "#b7b795",
-                "outline-style": "solid",
-                "outline-width": "3px",
-            },
-        ),
-    ],
-    style={"margin-left": "20px"},
-)
+                        ],
+                        id="modal",
+                        is_open=False,
+                    ),
+                ]
+            ),
+            # This is the div containing the anomalies data
+            html.Div(
+                children=[
+                    html.H5("Anomalies", style={"margin-top": "20px"}),
+                    dash_table.DataTable(
+                        id="InboxTable",
+                        columns=[{"name": i, "id": i} for i in actualDataDF.columns],
+                        editable=False,
+                        sort_action="native",
+                        sort_mode="multi",
+                        style_table={
+                            "overflow": "auto",
+                            "height": "100%",
+                            "width": "100%",
+                            "marginBottom": "20px",
+                        },
+                        style_header={
+                            "backgroundColor": "#b3b3b3",
+                            "fontWeight": "bold",
+                        },
+                        style_data={
+                            "whiteSpace": "normal",
+                            "width": "100px",
+                            "textAlign": "left",
+                        },
+                    ),
+                ],
+                style={
+                    "margin": "15px",
+                    "background-color": "#e0e0d1",
+                    "height": "8%",
+                    "width": "70vw",
+                    "outline-color": "#b7b795",
+                    "outline-style": "solid",
+                    "outline-width": "3px",
+                },
+            ),
+        ],
+        style={"margin-left": "20px"},
+    )
+
+
+layout = serve_layout
 
 
 # This is the callback for the functionality that marks/unmarks false positives in the anomaly data.
@@ -149,7 +161,15 @@ def openMarkerPopUp(active_cell, n, ok, value, data, is_open):
         if "demo-dropdown" == ctx.triggered_id:
             return is_open
         if "OK" == ctx.triggered_id:
-            selected = data[row]["ID"]
+            selected = data[row]["id"]
+            if value == "Mark as False Positive":
+                value = True
+            else:
+                value = False
+            requests.put(
+                "http://localhost:8002/anomalies/Update_false_positive",
+                params={"uId": selected, "uFalse_Positive": value},
+            )
             print(selected)
             print(value)
             return not is_open
@@ -157,6 +177,8 @@ def openMarkerPopUp(active_cell, n, ok, value, data, is_open):
             return not is_open
         elif col == "...":  # or whatever column you want
             return not is_open
+        elif col == "log_message":
+            return is_open
 
 
 def calculate_interval(value):
@@ -182,9 +204,34 @@ def calculate_interval(value):
     Input("interval_picker_dropdown", "value"),
 )
 def adjust_table_to_interval(value):
+    actualDataDF = getDataDF()
     interval = calculate_interval(value)
 
-    copyDf = originalDF[
-        (originalDF["Date"] <= interval[0]) & (originalDF["Date"] >= interval[1])
+    copyDf = actualDataDF[
+        (actualDataDF["log_time"] <= interval[0])
+        & (actualDataDF["log_time"] >= interval[1])
     ]
     return copyDf.to_dict(orient="records")
+
+
+def getDataDF():
+    data = requests.get("http://localhost:8002/anomalies/get_anomaly_list").json()
+    jsonData = json.dumps(data)
+    actualDataDF = pd.read_json(jsonData)
+    actualDataDF = actualDataDF.reindex(
+        columns=["id", "log_message", "log_time", "anomaly_score", "false_positive"]
+    )
+    buttonList = []
+    for i in actualDataDF.index:
+        buttonList.append("...")
+    actualDataDF["..."] = buttonList
+    pd.options.display.width = 10
+    return actualDataDF
+
+
+"""@callback(Output('url', 'pathname'),
+              [Input('OK', 'n_clicks')],
+              [State('url', 'pathname')])
+def navigate_to_anomalies(n_clicks, pathname):
+    if n_clicks:
+        return pathname"""
