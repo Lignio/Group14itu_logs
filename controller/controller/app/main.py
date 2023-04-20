d40dc5503659f6eabf05cdb87f1a6d6153b74d90
Lignio <olfw@itu.dk>
2023-04-20 11:42:10 +0200

    added css changes to backend/frontend code

--- controller/controller/app/main.py
+++ controller/controller/app/main.py
@@ -29,7 +29,15 @@ get_record = f"{data_generator}/logs/get_record"
 class Anomaly(BaseModel):
     log_time: str
     log_message: str
-    anomaly_score: float 
+    anomaly_score: float
+
+
+class FalsePositive(BaseModel):
+    log_time: str
+    log_message: str
+    anomaly_score: float
+    false_positive: bool
+
 
 # Function that calls the data-generator api for a list of the first 1000 logs and then calls the anomaly-detector api
 # to check which of those logs are a anomaly. It then returns a list of all anomalies and there anomaly-score.
@@ -51,56 +59,124 @@ def getAnomalyList(threshold: float = 0.02):
             myResult.append((i[0], anomalyScore))
     return myResult
 
+
 # Queue with our log messages
 logQueue = queue.Queue(1000)
 
+
 # Method for simlating getting constant steam of mesagges and inserting them into the queue
-def simulateLogsteam():
+def simulateLogstream():
     while True:
         # Missing load balancing when queue is full
         myLogmessage = requests.get(get_record).json()
-        logQueue.put(myLogmessage) # Put blocks/waits when the queue is full
-    
+        logQueue.put(myLogmessage)  # Put blocks/waits when the queue is full
 
-#Method for analysing and handeling log messages in the queue.
+
+# Method for analysing and handeling log messages in the queue.
 def simulateStreamAnalysis():
     while True:
         while logQueue.not_empty:
-            analysedMessage = requests.get(get_prediction, params={"log_message":logQueue.get(),"threshold":0.02 })
+            analysedMessage = requests.get(
+                get_prediction,
+                params={"log_message": logQueue.get(), "threshold": 0.02},
+            )
+            analysedMessage = analysedMessage.json()
+
+            dt = datetime.now()
+            dts = dt.strftime("%d/%m/%Y")
+
+            anomaly = Anomaly(
+                log_time=dts,
+                log_message=analysedMessage["log_message"],
+                anomaly_score=analysedMessage["anomaly_score"],
+            )
+
+            if analysedMessage["anomaly_score"] > 0.02:
+                # Replace print with insertion into database
+                # print(analysedMessage["log_message"])
+                new_post = Anomalies(**anomaly.dict())
+                data_writer.write_single_row_to_database(new_post)
+
+
+# Function to post an anomaly into db, by getting anomaly from stream and sending the parameters to data_generator, which handles the final insertion into Anomaly db table
+@app.post("/postAnomaly_stream")
+def postAnomaly():
+    while True:
+        while logQueue.not_empty:
+            myLogmessage = requests.get(get_record)
+            analysedMessage = requests.get(
+                get_prediction, params={"log_message": myLogmessage, "threshold": 0.02}
+            )
             analysedMessage = analysedMessage.json()
             if analysedMessage["anomaly_score"] > 0.02:
-                #Replace print with insertion into database
-                #print(analysedMessage["log_message"])
-                return post_anomaly(analysedMessage["log_message"], analysedMessage["anomaly_score"]) #changed post_test_anomaly to post_anomaly - may cause problems
-        return Anomaly(log_time="10", log_message=analysedMessage["log_message"], anomaly_score=analysedMessage["anomaly_score"])
+                # Replace print with insertion into database
+                # requests.post('http://localhost:8000/anomalies/post_anomaly', params={"log_message":analysedMessage["log_message"], "anomaly_score":analysedMessage["anomaly_score"] })
+                # print(analysedMessage["log_message"])
+                return post_test_anomaly(
+                    analysedMessage["log_message"], analysedMessage["anomaly_score"]
+                )
+            return Anomaly(
+                log_time="10",
+                log_message=analysedMessage["log_message"],
+                anomaly_score=analysedMessage["anomaly_score"],
+            )
 
 
 @app.get("/anomalies/get_anomaly_list")
 def get_anomaly_list():
-
     return data_loader.get_all_anomalies()
 
 
-#Endpoint for getting a log from the datagenerator, and the inserting into the db if it is an anomaly
-@app.post("/anomalies/post_anomaly",response_model=Anomaly)
+# Endpoint for getting a log from the datagenerator, and the inserting into the db if it is an anomaly
+@app.post("/anomalies/post_anomaly", response_model=Anomaly)
 def post_anomaly():
     myLogmessage = requests.get(get_record)
-    analysedMessage = requests.get(get_prediction, params={"log_message":myLogmessage,"threshold":0.02 })
+    analysedMessage = requests.get(
+        get_prediction, params={"log_message": myLogmessage, "threshold": 0.02}
+    )
     analysedMessage = analysedMessage.json()
 
     dt = datetime.now()
-    dts = dt.strftime('%d/%m/%Y')
-    
-    anomaly = Anomaly(log_time=dts, log_message=analysedMessage["log_message"], anomaly_score=analysedMessage["anomaly_score"])
+    dts = dt.strftime("%d/%m/%Y")
+
+    anomaly = Anomaly(
+        log_time=dts,
+        log_message=analysedMessage["log_message"],
+        anomaly_score=analysedMessage["anomaly_score"],
+    )
     if analysedMessage["anomaly_score"] > 0.02:
+        anomaly = Anomaly(
+            log_time=analysedMessage["log_time"],
+            log_message=analysedMessage["log_message"],
+            anomaly_score=analysedMessage["anomaly_score"],
+        )
         new_post = Anomalies(**anomaly.dict())
         data_writer.write_single_row_to_database(new_post)
     return anomaly
 
 
-t = threading.Thread(target=simulateLogsteam)
-t.daemon = True
-t.start()
-t2 = threading.Thread(target=simulateStreamAnalysis)
-t2.daemon = True
-t2.start()
+@app.put("/anomalies/Update_false_positive")
+def update_false_postive(uId: int, uFalse_Positive: bool):
+    anomaly = data_loader.get_Anomaly(uId)
+    data_writer.change_false_positive(anomaly, uFalse_Positive)
+
+
+@app.put("/anomalies/testUpdate_fp")
+def testUpdate_fp():
+    data_writer.test_fp()
+
+
+@app.post("/anomalies/testPost")
+def testPost():
+    data_writer.write_test()
+
+
+# Starts two threads, one simulation the log stream, the other simulating stream analysis
+@app.get("/anomalies/start_stream")
+def start_stream():
+    t = threading.Thread(target=simulateLogstream)
+    t.daemon = True
+    t.start()
+    t2 = threading.Thread(target=simulateStreamAnalysis)
+    t2.daemon = True
+    t2.start()
--- controller/controller/database/datawriter.py
+++ controller/controller/database/datawriter.py
@@ -1,9 +1,11 @@
 from sqlalchemy.future.engine import Engine
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 from controller.utils.sql_utils import get_database_engine
+from controller.database.tables import Logs, Anomalies
 
-class DataWriter():
+
+class DataWriter:
     def __init__(self):
         self.engine: Engine = get_database_engine()
 
@@ -12,10 +14,39 @@ class DataWriter():
         with Session(self.engine) as session:
             session.add(data)
             session.commit()
-    
+
     def write_multiple_rows_to_database(self, data: list):
         """Write multiple rows of data to database"""
         with Session(self.engine) as session:
             for i in data:
                 session.add(i)
+            session.commit()
+
+    def change_false_positive(self, data, falsePositive: bool):
+        anomaly = data
+        anomaly.false_positive = falsePositive
+        with Session(self.engine) as session:
+            session.add(anomaly)
+            session.commit()
+
+    def test_fp(self):
+        with Session(self.engine) as session:
+            statement = select(Anomalies).where(Anomalies.id == 1)
+            result = session.exec(statement)
+            anomaly = result.one()
+            anomaly.false_positive = True
+            session.add(anomaly)
+            session.commit()
+            session.refresh(anomaly)
+
+    def write_test(self):
+        data = Anomalies(
+            id=100069,
+            log_time="yee",
+            log_message="Yeeeeeeee",
+            anomaly_score=0.2,
+            false_positive=True,
+        )
+        with Session(self.engine) as session:
+            session.add(data)
             session.commit()
