7ada68556145e9b307338b97ed0e3fc47ae5f99f
Emilia-Victoria <emiliav.helsted@gmail.com>
2023-04-24 13:54:08 +0200

    Add css to alert

--- Dashboard_with_pages/app.py
+++ Dashboard_with_pages/app.py
@@ -34,29 +34,6 @@ app.layout = html.Div(
         html.Div(
             id="Sidebar",
             children=[
-                dbc.Alert(
-                    [
-                        html.H4(
-                            "New anomaly detected. \U0001f6d1",
-                            className="alert-heading",
-                        ),
-                        html.P("Choose to review it now or later."),
-                        html.Div(
-                            [
-                                dbc.Button("Dismis", id="dismis", className="ms-auto"),
-                                dbc.Button(
-                                    "Go to anomaly",
-                                    id="goTo",
-                                    className="ms-auto",
-                                    n_clicks=0,
-                                    href="/anomalies",
-                                ),
-                            ]
-                        ),
-                    ],
-                    id="alertMsg",
-                    is_open=False,  # Not sure if this line should be here.
-                ),
                 dcc.Interval(id="interval-component", interval=1 * 5000, n_intervals=0),
                 html.Div(
                     children=[
@@ -99,6 +76,30 @@ app.layout = html.Div(
             ],
         ),
         html.Div(id="Main-panel", children=[dash.page_container]),
+        dbc.Alert(
+                    [
+                        html.H4(
+                            "New anomaly detected. \U0001f6d1",
+                            className="alert-heading",
+                        ),
+                        html.P("Choose to review it now or later."),
+                        html.Div(
+                            [
+                                dbc.Button("Dismis", id="dismis", className="ms-auto"),
+                                dbc.Button(
+                                    "Go to anomaly",
+                                    id="goTo",
+                                    className="ms-auto",
+                                    n_clicks=0,
+                                    href="/anomalies",
+                                ),
+                            ]
+                        ),
+                    ],
+                    id="alertMsg",
+                    color="light",
+                    is_open=False,  # Not sure if this line should be here.
+                ),
     ],
     style={"display": "flex", "width": "100vw"},
 )
